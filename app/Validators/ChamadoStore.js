6a8adf5c6ee19bf7af1b5390102ff7e114d499ef
Jovanir Mateus de Almeida Lopes <jovamateus26@gmail.com>
2021-01-22 15:13:57 -0300

    Chamado

--- app/Validators/ChamadoStore.js
+++ app/Validators/ChamadoStore.js
@@ -4,18 +4,21 @@ class ChamadoStore {
   get rules () {
     return {
       // validation rules
+      dono: 'required',
+      assunto_id: 'required',
       titulo: 'required',
       texto: 'required',
-      assunto_id: 'required',
-      dono: 'required'
+
+
     }
   }
   get messages () {
     return {
+      'dono.required': 'Selecione o usuário solicitante',
       'titulo.required': 'O campo titulo não pode ser em branco',
-      'texto.required': 'O campo descrição completa não pode ser em branco',
       'assunto_id.required': 'Selecione um assunto',
-      'dono.required': 'Selecione o usuário solicitante'
+      'texto.required': 'O campo descrição completa não pode ser em branco'
+
     }
   }
 }
